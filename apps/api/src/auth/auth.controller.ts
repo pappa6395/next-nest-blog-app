eae2168b993e405de4bf807d340f5247411944e0
nontachai <pappaliving6395@gmail.com>
2025-03-11 01:19:18 +0700

    add dynamuc route to backend

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { GoogleAuthGuard } from './guards/google-auth/google-auth.guard';
 import { AuthService } from './auth.service';
 import { Response as Resp } from 'express';
 import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
+import { FRONTEND_URL } from '../constants';
 
 @Controller('auth')
 export class AuthController {
@@ -16,11 +17,10 @@ export class AuthController {
     @UseGuards(GoogleAuthGuard)
     @Get("google/callback")
     async googleCallback(@Request() req, @Response() res: Resp) {
-        //console.log('user:', req.user);
         const userData = await this.authService.login(req.user);
 
         res.redirect(
-            `http://localhost:3000/api/auth/google/callback?userId=${userData.id}&name=${userData.name}&avatar=${userData.avatar}&accessToken=${userData.accessToken}`);  // frontend API
+            `${FRONTEND_URL}/api/auth/google/callback?userId=${userData.id}&name=${userData.name}&avatar=${userData.avatar}&accessToken=${userData.accessToken}`);  // frontend API
     }
 
     @UseGuards(JwtAuthGuard)
--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -1,2 +1,4 @@
 
+export const FRONTEND_URL = process.env.FRONTEND_URL!;
+
 export const DEFAULT_PAGE_SIZE = 12;
